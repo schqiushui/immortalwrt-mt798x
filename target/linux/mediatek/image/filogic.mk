38698bf90a978e16e8e091e1d58cbd26d89334a7
schqiushui <orochi9999@gmail.com>
2026-03-27 23:51:50 +0800

    fix merge issue when compile
    Signed-off-by: schqiushui <orochi9999@gmail.com>

--- target/linux/mediatek/image/filogic.mk
+++ target/linux/mediatek/image/filogic.mk
@@ -2123,21 +2123,6 @@ define Device/totolink_x6000r
 endef
 TARGET_DEVICES += totolink_x6000r
 
-define Device/tplink_archer-ax80-v1
-  DEVICE_VENDOR := TP-Link
-  DEVICE_MODEL := Archer AX80V1
-  DEVICE_DTS := mt7986a-tplink-archer-ax80-v1
-  DEVICE_DTS_DIR := ../dts
-  DEVICE_PACKAGES := kmod-leds-lp5523 kmod-usb3 kmod-mt7915e \
-	kmod-mt7986-firmware mt7986-wo-firmware automount
-  UBINIZE_OPTS := -E 5
-  BLOCKSIZE := 128k
-  PAGESIZE := 2048
-  IMAGE_SIZE := 51200k
-  IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata
-endef
-TARGET_DEVICES += tplink_archer-ax80-v1
-
 define Device/bt_r320
   DEVICE_VENDOR := Globitel
   DEVICE_MODEL := BT-R320
